ebcc2d5437899c99f172aabf874a8a8e8428c437
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-08-26 20:12:26 -0400

    add bearer token to all requests after sign in

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
 export default function Dashboard() {
+  api.get('/appointments?page=1');
+
   return (
     <div>
       <h1>Dashboard</h1>
